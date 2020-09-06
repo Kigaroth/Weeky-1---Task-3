5327140f64cc815a090a031af14e6cc5ae974d5b
Kigaroth <hp_20@hotmail.com>
2020-09-06 13:30:01 +0200

    If statements in place, working out why it's not working all the way. Stops at "You are a grown up."

--- Weeky 1 - Task 3/Weeky 1 - Task 3.cpp
+++ Weeky 1 - Task 3/Weeky 1 - Task 3.cpp
@@ -5,27 +5,31 @@
 #include <conio.h>
 
 int age;
+bool sw = false;
 
 int a = 20;
 int b = 40;
 int c = 59;
+int d = 60;
 
 int main()
 {
-    std::cout << " Hello! May I ask how old you are? (type in your age)\n";
+    std::cout << " Hello!\n May I ask how old you are? (type in your age using numbers): ";
+
     std::cin >> age;
+
     if (age < a) {
         std::cout << " You are young.\n";
     }
-    else if (age > a || age == a) {
+
+    else if (age >= a || age == b) {
         std::cout << " You are a grown up.\n";
     }
-    else if (age > b) {
+
+    else if (age > b || age <= c) {
         std::cout << " You are old. \n";
     }
-    else (age < c) {
+    else if (age > d) {
         std::cout << " You ARE really old. \n";
     }
-    ;
 }
-
